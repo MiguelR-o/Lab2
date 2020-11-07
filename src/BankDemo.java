a2a12252c5636ed05ca926429154a03965995ceb
MiguelR-o <m.b.rosario@hotmail.com>
2020-11-07 20:47:28 +0000

    just a repo update nothing major added.

--- src/BankDemo.java
+++ src/BankDemo.java
@@ -2,6 +2,7 @@ import views.Cli;
 
 public class BankDemo {
     public static void main(String[] args) {
-
+        Cli cli = new Cli();
+        cli.start();
     }
 }
--- src/views/Cli.java
+++ src/views/Cli.java
@@ -1,7 +1,35 @@
 package views;
 
+import controllers.BankController;
+import java.util.Scanner;
+
 public class Cli {
+    BankController bank = new BankController();
+
     public void start() {
 
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String[] words = input.nextLine().split(" ");
+            if (input.nextLine() == "") {
+                break;
+            } else if (words[0] == "RC") {
+                while(true){
+                    Scanner secondLine = new Scanner(System.in);
+                    String[] word2 = input.nextLine().s
+                }
+                bank.registerClient();
+            } else if (words[0] == "AC") {
+
+            } else if (words[0] == "NC") {
+
+            } else if (words[0] == "M") {
+
+            } else if (words[0] == "SC") {
+
+            } else {
+                System.out.println("Instrução inválida.");
+            }
+        }
     }
 }
